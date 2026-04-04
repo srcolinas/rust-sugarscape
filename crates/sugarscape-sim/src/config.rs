8da414e4ec12afec155c341bdedbefb040237657
Sebastian Rodriguez Colina <srcolinas@gmail.com>
2026-04-04 07:15:32 -0500

    fix: cell capacity description of distribution follows Julia reference implementation

--- crates/sugarscape-sim/src/config.rs
+++ crates/sugarscape-sim/src/config.rs
@@ -14,23 +14,35 @@ pub struct WorldParams {
     pub width: u32,
     pub height: u32,
     pub growth_rate: u8,
-    pub capacity_distribution: Distribution,
-    pub initial_level_distribution: Distribution,
+    pub capacity_distribution: CellCapacityDistribution,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CellCapacityDistribution {
+    peaks: Vec<CellPosition>,
+    max_capacity: u8,
+    reduction_factor: f32,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct CellPosition {
+    pub x: u32,
+    pub y: u32,
 }
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "lowercase")]
-pub enum Distribution {
+pub enum RandomDistribution {
     Uniform { min: u32, max: u32 },
 }
 
 #[derive(Debug, Deserialize)]
 pub struct AgentParams {
     pub count: usize,
-    pub wealth_distribution: Distribution,
-    pub metabolic_rate_distribution: Distribution,
-    pub vision_distribution: Distribution,
-    pub max_age_distribution: Distribution,
+    pub wealth_distribution: RandomDistribution,
+    pub metabolic_rate_distribution: RandomDistribution,
+    pub vision_distribution: RandomDistribution,
+    pub max_age_distribution: RandomDistribution,
 }
 
 #[derive(Debug, Deserialize)]
